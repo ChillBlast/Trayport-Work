b9d2c23b8a3aeda4227df2425863f27ea0d4c308
Amir Saeidi <amirs@hq.trayport.com>
2018-07-26 16:15:52 +0100

    Added gello file

---
+++ CodeWars2/CodeWars2/kata9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+
+namespace CodeWars2
+{
+    public class Kata
+    {
+        public List<string> wave(string str)
+        {
+            return new List<string> { };
+        }
+    }
+}
