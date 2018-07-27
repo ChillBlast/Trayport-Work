9eee2f6def5c0dec9a1effbd1459ade2029f6f75
amirs <amirs@DEVLAP27>
2018-07-27 14:14:33 +0100

    14:14 27/07/2018

---
+++ CodeWars2/CodeWars2/QuickSort.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars2
+{
+    class QuickSort
+    {
+        public static bool validSort(int[] sortedArray)
+        {
+            for (int i = 0; i < sortedArray.Length-1; i++)
+            {
+                if (sortedArray[i] > sortedArray[i+1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static int[] QuickSortFuncFront(int[] unsortedArray)
+        {
+            return QuickSortFuncBack(unsortedArray);
+
+        }
+
+        private static int[] QuickSortFuncBack(int[] unsortedArray)
+        {
+            //if (unsortedArray.Length < 2) return unsortedArray;
+            List<int> sortedList = new List<int>();
+            List<int> sortedList1 = new List<int>();
+            List<int> sortedList2 = new List<int>();
+            for (int valid = 0; valid < 1; valid++)
+            {
+                int pivot = unsortedArray[unsortedArray.Length - 1];
+                //sortedList.Add(unsortedArray[unsortedArray.Length - 1]);
+                for (int i = 0; i < unsortedArray.Length-1; i++)
+                {
+                    if (unsortedArray[unsortedArray.Length - 1] < unsortedArray[i])
+                    {
+                        sortedList1.Add(unsortedArray[i]);
+                    }
+                    else
+                    {
+                        sortedList2.Insert(0, unsortedArray[i]);
+                    }
+                }
+                sortedList.AddRange(sortedList2);
+                sortedList1.Insert(0, pivot);
+                sortedList.AddRange(sortedList1);
+                int[] sortedArray = (sortedList).ToArray();
+                //sortedList1.Clear();
+                //sortedList2.Clear();
+                if (validSort(sortedArray))
+                {
+                    return sortedArray;
+                }
+                else
+                {
+                    //valid = -1;
+                    //unsortedArray = sortedArray;
+                    List<int> sortedArray1 = QuickSortFuncBack(sortedList1.ToArray()).ToList();
+                    List<int> sortedArray2 = QuickSortFuncBack(sortedList2.ToArray()).ToList();
+                    sortedArray2.AddRange(sortedArray1);
+                    return sortedArray2.ToArray();
+
+                }
+            }
+            int[] test = new int[10];
+            return test;
+        }
+    }
+}
---
+++ CodeWars2/CodeWars2/QuickSortTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeWars2;
+
+namespace CodeWars2
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class QuickSortTests
+    {
+        [Test]
+        public void Test()
+        {
+            int[] unsortedArray1 = new int[] { 1, 2, 4, 6, 8, 0, 9, 7, 5, 3 };
+            int[] sortedArray1 = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Assert.AreEqual(sortedArray1, QuickSort.QuickSortFuncFront(unsortedArray1));
+            int[] unsortedArray2 = new int[] { 1, 1, 1, 36, 4, 60, 49, 57, 0 };
+            int[] sortedArray2 = new int[] { 0, 1, 1, 1, 4, 36, 49, 57, 60 };
+            Assert.AreEqual(sortedArray2, QuickSort.QuickSortFuncFront(unsortedArray2));
+            int[] unsortedArray3 = new int[] { 0, 3, 1, 7 };
+            int[] sortedArray3 = new int[] { 0, 1, 3, 7 };
+            Assert.AreEqual(sortedArray3, QuickSort.QuickSortFuncFront(unsortedArray3));
+
+
+        }
+    }
+}
+
